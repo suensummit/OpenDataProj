bd20c5d0e4093a2ba3c92f9b2f9a172b619a1a54
Summit Suen <summit.suen@gmail.com>
2014-11-22 02:39:45 +0800

    update: parsing HR tables from 1990 to 2014

---
+++ parse_hr_tables_all.R
@@ -0,0 +1,22 @@
+library(XML)
+library(RSelenium)
+
+pJS <- phantom()
+Sys.sleep(5)
+remDr <- remoteDriver(browserName = 'phantomjs')
+remDr$open()
+
+url <- 'http://www.cpbl.com.tw/stats_hr.aspx'
+remDr$navigate(url)
+
+hr_tables <- list()
+
+year <- seq(1990, 2014)
+for (i in 1:length(year)) {
+  option <- remDr$findElement(using = 'xpath', paste0("//*/option[@value = '",year[i],"']"))
+  option$clickElement()
+  src <- option$getPageSource()
+  hr_tables[i] <- readHTMLTable(src[[1]], stringAsFactors = FALSE, encoding = "utf8")
+}
+
+View(hr_tables)
