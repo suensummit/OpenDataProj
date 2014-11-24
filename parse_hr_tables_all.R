01efcae7e81b4f3c2fdd6f8353e62677372b4567
Summit Suen <summit.suen@gmail.com>
2014-11-24 11:38:15 +0800

    update reprocessing hr_tables.RData

--- parse_hr_tables_all.R
+++ parse_hr_tables_all.R
@@ -23,3 +23,14 @@ for (i in 1:length(year)) {
 
 remDr$close()
 pJS$stop()
+
+year <- seq(1990, 2014)
+for (i in 1:length(year)) {
+  hr_table <- hr_tables[[i]]
+  names2 <- names(hr_table)
+  names2 <- c("NUMBER", "YEAR", "GID", "DATE", "STADIUM", "BATTER", "BATTERTEAM", "PITCHER", "PITCHERTEAM", "RBI", "REMARK")
+  names(hr_table) <- names2
+  hr_tables[[i]] <- hr_table
+}
+
+save(hr_tables, file = "Documents//OpenDataProj//hr_tables.RData")
--- report_hr_kmeans.R
+++ report_hr_kmeans.R
@@ -2,10 +2,7 @@ year <- seq(1990, 2014)
 for (i in 1:length(year)) {
   hr_table <- hr_tables[[i]]
   names2 <- names(hr_table)
-  names2 <- hr_table[1,]
-  names2[3] <- "GID"
-  names2[7] <- "BatterTeam"
-  names2[9] <- "PitcherTeam"
+  names2 <- c("NUMBER", "YEAR", "GID", "DATE", "STADIUM", "BATTER", "BATTERTEAM", "PITCHER", "PITCHERTEAM", "RBI", "REMARK")
   names(hr_table) <- names2
   hr_tables[[i]] <- hr_table
 }
