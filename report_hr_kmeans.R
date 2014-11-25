078b217c7d1a5baf357b1a561f8b7c2beeb4a13f
Summit Suen <summit.suen@gmail.com>
2014-11-25 17:14:46 +0800

    update

--- report_hr_kmeans.R
+++ report_hr_kmeans.R
@@ -1,8 +1,17 @@
+load("Documents//OpenDataProj//hr_tables.RData")
 year <- seq(1990, 2014)
 for (i in 1:length(year)) {
-  hr_table <- hr_tables[[i]]
-  names2 <- names(hr_table)
-  names2 <- c("NUMBER", "YEAR", "GID", "DATE", "STADIUM", "BATTER", "BATTERTEAM", "PITCHER", "PITCHERTEAM", "RBI", "REMARK")
-  names(hr_table) <- names2
-  hr_tables[[i]] <- hr_table
 }
+
+library(dplyr)
+TEAMS <- c()
+for (i in 1:length(year)) {
+  TEAMS <- union(TEAMS, levels(hr_tables[[i]]$BATTERTEAM))
+}
+
+
+hrs <- filter(hr_tables[[1]], BATTERTEAM == "三商")
+
+pat <- mutate(hr_tables[[1]], )
+
+patterns <- sapply(hr_tables, mutate())
